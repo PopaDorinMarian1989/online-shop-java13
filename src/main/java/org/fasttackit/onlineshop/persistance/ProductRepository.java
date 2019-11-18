d8f18ba61757bec89bf6871a87fe20b9f5e31e8f
popa dorin <dorinmarian89@gmail.com>
2019-11-18 21:33:18 +0200

    added product service and repository

---
+++ src/main/java/org/fasttackit/onlineshop/persistance/ProductRepository.java
@@ -0,0 +1,7 @@
+package org.fasttackit.onlineshop.persistance;
+
+import org.fasttackit.onlineshop.domain.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
---
+++ src/main/java/org/fasttackit/onlineshop/service/ProductService.java
@@ -0,0 +1,38 @@
+package org.fasttackit.onlineshop.service;
+
+import org.fasttackit.onlineshop.domain.Product;
+import org.fasttackit.onlineshop.persistance.ProductRepository;
+import org.fasttackit.onlineshop.transfer.SaveProductRequest;
+import org.slf4j.ILoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(ProductService.class);
+    // IoC - inversion of control
+    private final ProductRepository productRepository;
+
+   // dependency injection
+    @Autowired
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public Product createProduct(SaveProductRequest request){
+        LOGGER.info("Creating product{}", request);
+        Product product = new Product();
+        product.setDescription(request.getDescription());
+        product.setName(request.getName());
+        product.setPrice(request.getPrice());
+        product.setQuantity(request.getQuantity());
+        product.setImageUrl(request.getImageUrl());
+
+        return productRepository.save(product);
+
+    }
+}
